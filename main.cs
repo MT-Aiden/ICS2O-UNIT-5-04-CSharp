267b1236d1b32abc229aa2a1eab07309eb2c501c
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-05-09 16:17:36 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,60 @@
+// Aiden McLeod
+// ICS2O-Unit5-04-CSharp
+// May 3 2022
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        float age;
+        int dayint;
+        string day;
+
+        void askAge()
+        {
+            Console.Write("Enter age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+
+        void askDay()
+        {
+            Console.WriteLine("Enter day of the week: ");
+            Console.WriteLine("1 - Sunday");
+            Console.WriteLine("2 - Monday");
+            Console.WriteLine("3 - Tuesday");
+            Console.WriteLine("4 - Wednesday");
+            Console.WriteLine("5 - Thursday");
+            Console.WriteLine("6 - Friday");
+            Console.WriteLine("7 - Saturday");
+            dayint = Convert.ToInt32(Console.ReadLine());
+            if (dayint > 7 || dayint < 1)
+            {
+                Console.Clear();
+                Console.WriteLine("\nPlease enter a valid number.");
+                askDay();
+            }
+            else if (dayint == 3 || dayint == 5)
+            {
+                day = "Free Day";
+            }
+            else
+            {
+                day = "Unfree Day";
+            }
+        }
+
+        askAge();
+        askDay();
+        if (day == "Free Day" && day != "Unfree Day" || age > 12 && age < 22)
+        {
+            Console.WriteLine("\nYou can pay with the museum's student pricing.");
+        }
+        else
+        {
+            Console.WriteLine("\nYou must pay with the museum's regular pricing.");
+        }
+        Console.WriteLine("\nFinished.");
+    }
+}
